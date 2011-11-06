otherlab/pcl

commit 4f48c2ae2a3f3d160e1464c1172b23c32f9c746d

* Fixed a bug in @PointCloudColorHandlerRGBField<sensor_msgs::PointCloud2>@ where one of the offsets for Z was incorrectly calculated thus leading to erroneous data (#404 - thanks Lucas!)
git-svn-id: http://svn.pointclouds.org/pcl/trunk@3112 a9d63959-f2ad-4865-b262-bf0e56cfafb6

visualization/src/point_cloud_handlers.cpp
@@ -135,10 +135,13 @@ pcl::visualization::PointCloudColorHandlerRGBField<sensor_msgs::PointCloud2>::ge
   
   // If XYZ present, check if the points are invalid
   int x_idx = pcl::getFieldIndex (*cloud_, "x");
+  int x_point_offset = cloud_->fields[x_idx].offset,
+      y_point_offset = x_point_offset + sizeof (float),
+      z_point_offset = y_point_offset + sizeof (float);
+
   if (x_idx != -1)
   {
     float x_data, y_data, z_data;
-    int x_point_offset = cloud_->fields[x_idx].offset;
     
     // Color every point
     for (vtkIdType cp = 0; cp < nr_points; ++cp, 
@@ -152,8 +155,8 @@ pcl::visualization::PointCloudColorHandlerRGBField<sensor_msgs::PointCloud2>::ge
         continue;
 
       memcpy (&x_data, &cloud_->data[x_point_offset], sizeof (float));
-      memcpy (&y_data, &cloud_->data[x_point_offset + sizeof (float)], sizeof (float));
-      memcpy (&z_data, &cloud_->data[x_point_offset + sizeof (float)], sizeof (float));
+      memcpy (&y_data, &cloud_->data[y_point_offset], sizeof (float));
+      memcpy (&z_data, &cloud_->data[z_point_offset], sizeof (float));
 
       if (!pcl_isfinite (x_data) || !pcl_isfinite (y_data) || !pcl_isfinite (z_data))
         continue;
@@ -222,10 +225,13 @@ pcl::visualization::PointCloudColorHandlerGenericField<sensor_msgs::PointCloud2>
 
   // If XYZ present, check if the points are invalid
   int x_idx = pcl::getFieldIndex (*cloud_, "x");
+  int x_point_offset = cloud_->fields[x_idx].offset,
+      y_point_offset = x_point_offset + sizeof (float),
+      z_point_offset = y_point_offset + sizeof (float);
+
   if (x_idx != -1)
   {
     float x_data, y_data, z_data;
-    int x_point_offset = cloud_->fields[x_idx].offset;
     
     // Color every point
     for (vtkIdType cp = 0; cp < nr_points; ++cp,
@@ -240,8 +246,9 @@ pcl::visualization::PointCloudColorHandlerGenericField<sensor_msgs::PointCloud2>
         continue;
 
       memcpy (&x_data, &cloud_->data[x_point_offset], sizeof (float));
-      memcpy (&y_data, &cloud_->data[x_point_offset + sizeof (float)], sizeof (float));
-      memcpy (&z_data, &cloud_->data[x_point_offset + sizeof (float)], sizeof (float));
+      memcpy (&y_data, &cloud_->data[y_point_offset], sizeof (float));
+      memcpy (&z_data, &cloud_->data[z_point_offset], sizeof (float));
+
       if (!pcl_isfinite (x_data) || !pcl_isfinite (y_data) || !pcl_isfinite (z_data))
         continue;
 

commit 9932a4ed93f414754c8bad80f5f29e5b77043cac

Fix bug in @PointCloudColorHandlerRGBField<sensor_msgs::PointCloud2>@ introduced in r3112.
y_point_offset and z_point_offset where not incremented.


git-svn-id: http://svn.pointclouds.org/pcl/trunk@3115 a9d63959-f2ad-4865-b262-bf0e56cfafb6

visualization/src/point_cloud_handlers.cpp
@@ -135,13 +135,10 @@ pcl::visualization::PointCloudColorHandlerRGBField<sensor_msgs::PointCloud2>::ge
   
   // If XYZ present, check if the points are invalid
   int x_idx = pcl::getFieldIndex (*cloud_, "x");
-  int x_point_offset = cloud_->fields[x_idx].offset,
-      y_point_offset = x_point_offset + sizeof (float),
-      z_point_offset = y_point_offset + sizeof (float);
-
   if (x_idx != -1)
   {
     float x_data, y_data, z_data;
+    int x_point_offset = cloud_->fields[x_idx].offset;
     
     // Color every point
     for (vtkIdType cp = 0; cp < nr_points; ++cp, 
@@ -155,8 +152,8 @@ pcl::visualization::PointCloudColorHandlerRGBField<sensor_msgs::PointCloud2>::ge
         continue;
 
       memcpy (&x_data, &cloud_->data[x_point_offset], sizeof (float));
-      memcpy (&y_data, &cloud_->data[y_point_offset], sizeof (float));
-      memcpy (&z_data, &cloud_->data[z_point_offset], sizeof (float));
+      memcpy (&y_data, &cloud_->data[x_point_offset + sizeof (float)], sizeof (float));
+      memcpy (&z_data, &cloud_->data[x_point_offset + 2 * sizeof (float)], sizeof (float));
 
       if (!pcl_isfinite (x_data) || !pcl_isfinite (y_data) || !pcl_isfinite (z_data))
         continue;
@@ -225,13 +222,10 @@ pcl::visualization::PointCloudColorHandlerGenericField<sensor_msgs::PointCloud2>
 
   // If XYZ present, check if the points are invalid
   int x_idx = pcl::getFieldIndex (*cloud_, "x");
-  int x_point_offset = cloud_->fields[x_idx].offset,
-      y_point_offset = x_point_offset + sizeof (float),
-      z_point_offset = y_point_offset + sizeof (float);
-
   if (x_idx != -1)
   {
     float x_data, y_data, z_data;
+    int x_point_offset = cloud_->fields[x_idx].offset;
     
     // Color every point
     for (vtkIdType cp = 0; cp < nr_points; ++cp,
@@ -246,9 +240,8 @@ pcl::visualization::PointCloudColorHandlerGenericField<sensor_msgs::PointCloud2>
         continue;
 
       memcpy (&x_data, &cloud_->data[x_point_offset], sizeof (float));
-      memcpy (&y_data, &cloud_->data[y_point_offset], sizeof (float));
-      memcpy (&z_data, &cloud_->data[z_point_offset], sizeof (float));
-
+      memcpy (&y_data, &cloud_->data[x_point_offset + sizeof (float)], sizeof (float));
+      memcpy (&z_data, &cloud_->data[x_point_offset + 2 * sizeof (float)], sizeof (float));
       if (!pcl_isfinite (x_data) || !pcl_isfinite (y_data) || !pcl_isfinite (z_data))
         continue;
 