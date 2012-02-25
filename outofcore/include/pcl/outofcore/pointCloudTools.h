a8084ceaf9a30e9f51498e5d598cadf18cd2aa4c
stfox88 <stfox88@a9d63959-f2ad-4865-b262-bf0e56cfafb6>
2012-02-25 18:23:30 +0000

    outofcore: refactored, added copyright headers, wrapped into namespace pcl::outofcore, fixed unit tests
    git-svn-id: http://svn.pointclouds.org/pcl/trunk@4732 a9d63959-f2ad-4865-b262-bf0e56cfafb6

--- outofcore/include/pcl/outofcore/pointCloudTools.h
+++ outofcore/include/pcl/outofcore/pointCloudTools.h
@@ -1,32 +1,40 @@
-#ifndef PCL_OUTOFCORE_POINTCLOUDTOOLS_H_
-#define PCL_OUTOFCORE_POINTCLOUDTOOLS_H_
-
 /*
-  Copyright (c) 2012, Urban Robotics Inc
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-  * Redistributions of source code must retain the above copyright
-  notice, this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright
-  notice, this list of conditions and the following disclaimer in the
-  documentation and/or other materials provided with the distribution.
-  * Neither the name of Urban Robotics Inc nor the
-  names of its contributors may be used to endorse or promote products
-  derived from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Software License Agreement (BSD License)
+ *
+ *  Point Cloud Library (PCL) - www.pointclouds.org
+ *  Copyright (c) 2010-2012, Willow Garage, Inc.
+ *  Copyright (c) 2009-2012, Urban Robotics, Inc.
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 /*
   This code defines the point used for point storage at Urban Robotics. Please
@@ -38,37 +46,46 @@
 //Stephen Bekefi
 //Urban Robotics 2009
 
-namespace PointCloudTools
-{
-  struct point
-  {
-    double x;
-    double y;
-    double z;
-    double nx;
-    double ny;
-    double nz;
-    double r;
-    double g;
-    double b;
-    double error;
-    unsigned int cameraCount;
-    unsigned int triadID;
+#ifndef PCL_OUTOFCORE_POINTCLOUDTOOLS_H_
+#define PCL_OUTOFCORE_POINTCLOUDTOOLS_H_
 
-    bool operator==(const point& rhs) const
+namespace pcl
+{
+  namespace octree
+  {
+    namespace PointCloudTools
+    {
+      struct point
       {
-        if (x == rhs.x && y == rhs.y && z == rhs.z &&
-            nx == rhs.nx && ny == rhs.ny && nz == rhs.nz &&
-            r == rhs.r && g == rhs.g && b == rhs.b &&
-            error == rhs.error &&
-            cameraCount == rhs.cameraCount &&
-            triadID == rhs.triadID)
-        {
-          return true;
-        }
-        return false;
-      }
-  };
+        double x;
+        double y;
+        double z;
+        double nx;
+        double ny;
+        double nz;
+        double r;
+        double g;
+        double b;
+        double error;
+        unsigned int cameraCount;
+        unsigned int triadID;
+
+        bool operator==(const point& rhs) const
+          {
+            if (x == rhs.x && y == rhs.y && z == rhs.z &&
+                nx == rhs.nx && ny == rhs.ny && nz == rhs.nz &&
+                r == rhs.r && g == rhs.g && b == rhs.b &&
+                error == rhs.error &&
+                cameraCount == rhs.cameraCount &&
+                triadID == rhs.triadID)
+            {
+              return true;
+            }
+            return false;
+          }
+      };
+    }
+  }
 }
 
 #endif //PCL_OUTOFCORE_POINTCLOUDTOOLS_H_
